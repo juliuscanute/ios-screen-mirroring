1e77f1e2570ffe471554be99eec99baec4767d3d
Julius Canute <juliuscanute@gmail.com>
2025-03-26 13:56:48 +0530

    Initial Commit

---
+++ ScreenMirroring/ScreenMirroringApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenMirroringApp.swift
+//  ScreenMirroring
+//
+//  Created by Julius Canute on 26/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenMirroringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreenMirroringTests/ScreenMirroringTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenMirroringTests.swift
+//  ScreenMirroringTests
+//
+//  Created by Julius Canute on 26/3/2025.
+//
+
+import Testing
+@testable import ScreenMirroring
+
+struct ScreenMirroringTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
